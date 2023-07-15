df85db3473b2250f5a8c9b1b258f182c87f0ebe8
Learistocats <lea.guechoum@gmail.com>
2023-07-15 19:44:21 +0200

    Added login form

--- js/main.js
+++ js/main.js
@@ -2,9 +2,26 @@ window.onload = function () {
 
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
+    const loginBtn = document.getElementById('login-btn');
+    const loginBtnMobile = document.getElementById('login-btn-mobile');
+    const loginFormContainer = document.getElementById('login-form-container');
+    const closeBtn = document.getElementById('close-btn');
+
 
     menu_btn.addEventListener('click', function () {
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
+    });
+
+    loginBtn.addEventListener('click', function() {
+        loginFormContainer.style.display = 'block';
+    });
+
+    loginBtnMobile.addEventListener('click', function() {
+        loginFormContainer.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', function() {
+        loginFormContainer.style.display = 'none';
     });
 }
